a94c72e4f9e5f0a27898fb2fcfe959876f3b10cd
littleBitsman <67122660+littleBitsman@users.noreply.github.com>
2023-05-31 16:24:15 +0000

    no spoilers

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function doVideo(unlucky) {
     }
     document.getElementById("appearOnPress-h5").innerHTML = "on mobile and tablet devices it doesnt autoplay :("
     document.getElementById("funnystuff").style.color = "#ff0000"
-    document.getElementById("guiding-light").style.color = "#B7EBF4"
+    document.getElementById("guiding-light").style.color = "#77ABB4"
     document.getElementById("guiding-light").innerHTML = guidinglight[Math.floor(Math.random() * guidinglight.length)]
     const videoId = lol.replace("https://youtu.be/", "")
     new YT.Player('video', {
